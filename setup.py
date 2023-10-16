c3995d707c469edec2c82e60ba4c2d45473ba8f9
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-10-16 17:01:31 +0200

    start upgrading to black-pack

--- setup.py
+++ setup.py
@@ -6,21 +6,19 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name='ray_voxel_overlap',
+    name="ray_voxel_overlap",
     version="0.0.4",
-    author='Sebastian Achim Mueller',
-    author_email='sebastian-achim.mueller@mpi-hd.mpg.de',
-    description='Estimate the tomographic system-matrix for rays in voxels.',
+    author="Sebastian Achim Mueller",
+    author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
+    description="Estimate the tomographic system-matrix for rays in voxels.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/cherenkov-plenoscope/ray_voxel_overlap',
-    packages=[
-        'ray_voxel_overlap'
-    ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/cherenkov-plenoscope/ray_voxel_overlap",
+    packages=["ray_voxel_overlap"],
     install_requires=[
-        'setuptools>=18.0',
-        'cython',
-        'scipy',
+        "setuptools>=18.0",
+        "cython",
+        "scipy",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -34,15 +32,17 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Scientific/Engineering :: Astronomy",
     ],
-    python_requires='>=3',
+    python_requires=">=3",
     ext_modules=[
         setuptools.Extension(
             "ray_voxel_overlap._cython_overlap",
             sources=[
                 os.path.join(
-                    'ray_voxel_overlap', '_cython_overlap_implementation.pyx'),
+                    "ray_voxel_overlap", "_cython_overlap_implementation.pyx"
+                ),
                 os.path.join(
-                    'ray_voxel_overlap', '_c_overlap_implementation.c'),
+                    "ray_voxel_overlap", "_c_overlap_implementation.c"
+                ),
             ],
             include_dirs=[numpy.get_include(), "ray_voxel_overlap"],
             language="c",
