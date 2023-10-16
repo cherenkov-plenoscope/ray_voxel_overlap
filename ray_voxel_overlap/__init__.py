bf9a052c8bda1ad85e4bd458cddfe4c5fe8c9051
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-10-16 17:15:28 +0200

    version in code

--- ray_voxel_overlap/__init__.py
+++ ray_voxel_overlap/__init__.py
@@ -8,6 +8,7 @@ Supported by:
 Estimate the eucledian overlap of rays and volume-cells (voxels).
 Estimate the tomographic system-matrix for your detector.
 """
+from .version import __version__
 from . import _cython_overlap
 from . import _py_overlap
 from ._cython_overlap import estimate_overlap_of_ray_with_voxels
--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@ import setuptools
 import numpy
 import os
 
-with open("README.md", "r") as f:
+with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
+
+
+with open(os.path.join("ray_voxel_overlap", "version.py")) as f:
+    txt = f.read()
+    last_line = txt.splitlines()[-1]
+    version_string = last_line.split()[-1]
+    version = version_string.strip("\"'")
+
 
 setuptools.setup(
     name="ray_voxel_overlap",
-    version="0.0.4",
+    version=version,
     author="Sebastian Achim Mueller",
     author_email="sebastian-achim.mueller@mpi-hd.mpg.de",
     description="Estimate the tomographic system-matrix for rays in voxels.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/cherenkov-plenoscope/ray_voxel_overlap",
     packages=["ray_voxel_overlap"],
     install_requires=[
@@ -20,6 +28,7 @@ setuptools.setup(
         "cython",
         "scipy",
     ],
+    package_data={"ray_voxel_overlap": []},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: C",
